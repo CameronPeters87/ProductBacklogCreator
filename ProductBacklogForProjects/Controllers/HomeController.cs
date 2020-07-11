1910052960ba757410c3f161e35305ef794526a4
cameronpeters87 <cameronpeters87@gmail.com>
2020-07-11 11:30:57 +0200

    Added Edit and Delete of Projects

--- ProductBacklogForProjects/Controllers/HomeController.cs
+++ ProductBacklogForProjects/Controllers/HomeController.cs
@@ -82,15 +82,15 @@ namespace ProductBacklogForProjects.Controllers
             return View(project);
         }
         [Authorize]
-        public async Task<ActionResult> EditProject(int? id)
+        public async Task<ActionResult> EditProject(int projectId)
         {
             var db = ApplicationDbContext.Create();
 
-            if (id == null)
+            if (projectId == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Project project = await db.Projects.FindAsync(id);
+            Project project = await db.Projects.FindAsync(projectId);
             if (project == null)
             {
                 return HttpNotFound();
@@ -122,15 +122,15 @@ namespace ProductBacklogForProjects.Controllers
 
         // GET: Admin/Subjects/Delete/5
         [Authorize]
-        public async Task<ActionResult> DeleteProject(int? id)
+        public async Task<ActionResult> DeleteProject(int projectId)
         {
             var db = ApplicationDbContext.Create();
 
-            if (id == null)
+            if (projectId == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Project project = await db.Projects.FindAsync(id);
+            Project project = await db.Projects.FindAsync(projectId);
             if (project == null)
             {
                 return HttpNotFound();
@@ -142,13 +142,13 @@ namespace ProductBacklogForProjects.Controllers
         [Authorize]
         [HttpPost, ActionName("DeleteProject")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int projectId)
         {
             var db = ApplicationDbContext.Create();
 
-            Project project = await db.Projects.FindAsync(id);
+            Project project = await db.Projects.FindAsync(projectId);
             var products = await (from p in db.Products
-                                  where p.ProjectId == id
+                                  where p.ProjectId == projectId
                                   select p).ToListAsync();
 
             db.Projects.Remove(project);
@@ -168,142 +168,5 @@ namespace ProductBacklogForProjects.Controllers
             }
             base.Dispose(disposing);
         }
-        [Authorize]
-        public async Task<ActionResult> ProductBacklog(int projectId)
-        {
-            if (projectId == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            var db = ApplicationDbContext.Create();
-            var model = new ProjectProductsViewModel();
-
-            model.ProductModels = await (from p in db.Products
-                                         join pro in db.Projects on p.ProjectId equals pro.Id
-                                         join s in db.Subjects on p.SubjectId equals s.Id
-                                         join st in db.Statuses on p.StatusId equals st.Id
-                                         join pr in db.Priorities on p.PriorityId equals pr.Id
-                                         where p.ProjectId.Equals(projectId)
-                                         select new ProductModel
-                                         {
-                                             Id = p.Id,
-                                             Goal = p.Goal,
-                                             Benefit = p.Benefit,
-                                             Sprint = p.Sprint,
-                                             StatusId = p.StatusId,
-                                             ProjectId = p.ProjectId,
-                                             PriorityId = p.PriorityId,
-                                             SubjectId = p.SubjectId,
-                                             SubjectName = s.Name,
-                                             PriorityName = pr.Name,
-                                             StatusName = st.Name,
-                                             ProjectName = pro.Name
-                                         }).ToListAsync();
-
-            // For dropdownlists
-            model.Subjects = await db.Subjects.ToListAsync();
-            model.Priorities = await db.Priorities.ToListAsync();
-            model.Statuses = await db.Statuses.ToListAsync();
-            model.ProjectId = projectId;
-
-            //model.ProjectName = await (from p in db.Projects
-            //                           where p.Id.Equals(projectId)
-            //                           select p.Name).FirstOrDefaultAsync();
-            ViewBag.ProjectName = "Hello";
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public async Task<ActionResult> ProductBacklog(ProjectProductsViewModel model)
-        {
-            try
-            {
-                if (model == null)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-                if (ModelState.IsValid)
-                {
-                    var db = new ApplicationDbContext();
-                    db.Products.Add(new Product
-                    {
-                        Id = model.Id,
-                        PriorityId = model.PriorityId,
-                        ProjectId = model.ProjectId,
-                        SubjectId = model.SubjectId,
-                        StatusId = model.StatusId,
-                        Benefit = model.Benefit,
-                        Goal = model.Goal,
-                        Sprint = model.Sprint,
-                    });
-                    await db.SaveChangesAsync();
-                }
-            }
-            catch
-            {
-
-            }
-            return RedirectToAction("ProductBacklog", "Home",
-                new { projectId = model.ProjectId });
-        }
-
-        public async Task<ActionResult> RemoveProduct(int projectId, int productId)
-        {
-            try
-            {
-                if (projectId == null || productId <= 0)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-                if (ModelState.IsValid)
-                {
-                    var db = new ApplicationDbContext();
-                    var prods = db.Products.Where(p => p.Id.Equals(productId) &&
-                        p.ProjectId.Equals(projectId));
-                    db.Products.RemoveRange(prods);
-                    await db.SaveChangesAsync();
-                }
-            }
-            catch
-            {
-
-            }
-            return RedirectToAction("ProductBacklog", "Home",
-                new { projectId = projectId });
-        }
-        //[Authorize]
-        //public async Task<ActionResult> EditProduct(int id)
-        //{
-        //    if (id <= 0)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    var db = ApplicationDbContext.Create();
-        //    Product product = await db.Products.FindAsync(id);
-        //    //Product product = db.Products.Where(p => p.Id.Equals(productId) &&
-        //    //    p.ProjectId.Equals(projectId)).First();
-
-
-
-        //    if (product == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    var prod = new List<Product>();
-        //    prod.Add(product);
-        //    var ProductModel = await prod.Convert(db);
-
-        //    // For dropdownlists
-
-        //    return View(ProductModel.First());
-        //    //return View(product);
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //[Authorize]
-
     }
 }
